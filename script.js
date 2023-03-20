c015fb2b459cccaffc8ae81a3c36f3c4bd5af6f8
Programmer-Mehadi <mehadi.developer@gmail.com>
2023-03-20 21:40:35 +0600

    ui design and fullscreen and exit fullscreen done.

---
+++ script.js
@@ -0,0 +1,27 @@
+const seeFullScreenBtn = document.getElementById('see_fullscreen');
+const exitFullScreenBtn = document.getElementById('exit_fullscreen');
+
+seeFullScreenBtn.addEventListener('click', () => {
+    const stopwatch_body = document.getElementById('stopwatch_body');
+    if (stopwatch_body.requestFullscreen) {
+        stopwatch_body.requestFullscreen();
+      } else if (stopwatch_body.webkitRequestFullscreen) { /* Safari */
+        stopwatch_body.webkitRequestFullscreen();
+      } else if (stopwatch_body.msRequestFullscreen) { /* IE11 */
+        stopwatch_body.msRequestFullscreen();
+    }
+    stopwatch_body.style.height = '100vh';
+    document.getElementsByClassName('content')[0].style.flexGrow = '1';
+})
+exitFullScreenBtn.addEventListener('click', () => {
+    const stopwatch_body = document.getElementById('stopwatch_body');
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+      }
+    stopwatch_body.style.height = 'fit-content';
+    document.getElementsByClassName('content')[0].style.flexGrow = '1';
+})
